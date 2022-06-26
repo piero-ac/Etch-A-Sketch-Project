d6735c479774f3ccabd25e9b2e6cddb5641416be
piero-ac <pacoronado01@gmail.com>
2022-06-25 21:10:40 -0400

    Add coloring mode state variable and clearBoard function

--- script.js
+++ script.js
@@ -2,35 +2,53 @@ const sketchBoard = document.getElementById("sketch-board");
 const gridSizePicker = document.getElementById("gridsize-picker");
 const gridSizeLabel = document.getElementById("gridsize-label");
 const colorPicker = document.getElementById("color-picker");
+const colorModeButton = document.getElementById("color-mode-btn");
 const clearButton = document.getElementById("clear-btn");
 const eraserButton = document.getElementById("eraser-btn");
 const sketchBoardWidth = 500;
 
 let sketchingColor = colorPicker.value;
 let currentGridSize = parseInt(gridSizePicker.value);
-let erasing = false;
+let coloringMode = true;
+let erasingMode = false;
 
 // Run the populateGrid function on window load
 window.onload = populateGrid(currentGridSize);
 
 // Grid Size Picker event listener to update the text showing current grid size
 gridSizePicker.addEventListener("change", function() {
-    erasing = false;
+    coloringMode = true;
+    erasingMode = false;
     currentGridSize = gridSizePicker.value;
     gridSizeLabel.textContent = currentGridSize + " x " + currentGridSize;
 });
 
 // Event listener to update the number of square in the sketchboard
+gridSizePicker.addEventListener("change", function(){
+  coloringMode = true;
+  erasingMode = false;
+  unpopulateGrid();
+  populateGrid(currentGridSize);
+});
 
 // Event listener to clear the sketchboard
 clearButton.addEventListener("click", clearBoard);
 
-// Event listener to erase the color of the specified grid
+// Event listener to enter erasing mode
 eraserButton.addEventListener("click", function() {
-    erasing = true;
+    erasingMode = true;
+    coloringMode = false;
 });
 
+// Event listener to enter coloring mode
+colorModeButton.addEventListener("click", function() {
+  erasingMode = false;
+  coloringMode = true;
+});
 
+function unpopulateGrid() {
+  sketchBoard.innerHTML = '';
+}
 
 function populateGrid(currentGridSize) {
     // Use for loop to create necessary number of grid squares
@@ -49,7 +67,7 @@ function createGridSquare(currentGridSize){
     gridSquare.style.height = gridSquareHeightAndWidth + "px";
     gridSquare.style.width = gridSquareHeightAndWidth + "px";
     gridSquare.addEventListener("mouseover", function(event) {
-        if(erasing) {
+        if(erasingMode && !coloringMode) {
           event.target.style.backgroundColor = "whitesmoke";
         } else {
           event.target.style.backgroundColor = sketchingColor;  
@@ -60,13 +78,11 @@ function createGridSquare(currentGridSize){
 }
 
 function clearBoard(){
-    erasing = false;
+  coloringMode = true;
+    erasingMode = false;
     const gridSquares = sketchBoard.childNodes;
     gridSquares.forEach(gridSquare => {
         gridSquare.style.backgroundColor = "whitesmoke";
     });
 }
 
-// function eraseColor(event){
-//     target.
-// }
