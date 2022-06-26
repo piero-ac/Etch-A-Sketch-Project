c9324e52c3541812afb7fee8441d777073e1e983
piero-ac <pacoronado01@gmail.com>
2022-06-25 20:03:20 -0400

    Create populateGrid and createGridSquare functions

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
+const sketchBoard = document.getElementById("sketch-board");
 const gridSizePicker = document.getElementById("gridsize-picker");
 const gridSizeLabel = document.getElementById("gridsize-label");
 
+const sketchBoardWidth = 500;
+let currentGridSize = parseInt(gridSizePicker.value);
+
+// Run the populateGrid function on window load
+window.onload = populateGrid(currentGridSize);
+
 // Grid Size Picker event listener to update the text showing current grid size
 gridSizePicker.addEventListener("change", function() {
-    let currentGridSize = gridSizePicker.value;
+    currentGridSize = gridSizePicker.value;
     gridSizeLabel.textContent = currentGridSize + " x " + currentGridSize;
 });
+
+function populateGrid(currentGridSize) {
+    // 1. use for loop to create necessary number of grid squares
+    // and add them to the sketch board
+    const numberOfSquares = currentGridSize**2;
+    for(let i = 0; i < numberOfSquares; i++){
+        const gridSquare = createGridSquare(currentGridSize);
+        sketchBoard.appendChild(gridSquare);
+    }
+
+}
+
+function createGridSquare(currentGridSize){
+    const gridSquareHeightAndWidth = sketchBoardWidth / currentGridSize;
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.add("grid-square");
+    gridSquare.style.height = gridSquareHeightAndWidth + "px";
+    gridSquare.style.width = gridSquareHeightAndWidth + "px";
+    
+    // gridSquare.style.border = "1px solid black";
+    return gridSquare;
+}
